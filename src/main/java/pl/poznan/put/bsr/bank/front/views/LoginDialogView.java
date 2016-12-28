e21cecb2ab3e03198971f403398324b10f9c25b3
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-12-28 17:49:36 +0100

    Added login dialog view class

---
+++ src/main/java/pl/poznan/put/bsr/bank/front/views/LoginDialogView.java
@@ -0,0 +1,34 @@
+package pl.poznan.put.bsr.bank.front.views;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+/**
+ * @author Kamil Walkowiak
+ */
+public class LoginDialogView {
+    private Stage stage;
+
+    public LoginDialogView() throws IOException {
+        stage = new Stage();
+        stage.initStyle(StageStyle.DECORATED);
+        stage.setResizable(false);
+        stage.initModality(Modality.NONE);
+        stage.setTitle("eBank - Login");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/LoginDialog.fxml"));
+        Parent root = fxmlLoader.load();
+
+        stage.setScene(new Scene(root));
+    }
+
+    public void show() {
+        stage.show();
+    }
+}
