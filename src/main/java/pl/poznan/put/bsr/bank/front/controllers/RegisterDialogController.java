26381a5cfdadbd9a7a7f71d63ea86ca2ea20693d
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-12-28 18:51:05 +0100

    Added register dialog

---
+++ src/main/java/pl/poznan/put/bsr/bank/front/controllers/RegisterDialogController.java
@@ -0,0 +1,48 @@
+package pl.poznan.put.bsr.bank.front.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import pl.poznan.put.bsr.bank.front.utils.BankServiceUtil;
+import pl.poznan.put.bsr.bank.services.BankServiceException_Exception;
+import pl.poznan.put.bsr.bank.services.ValidationException_Exception;
+
+/**
+ * @author Kamil Walkowiak
+ */
+public class RegisterDialogController {
+    @FXML
+    private TextField loginTextField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField firstNameTextField;
+    @FXML
+    private TextField lastNameTextField;
+    @FXML
+    private Button registerButton;
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private void handleRegisterButton() {
+        String login = loginTextField.getText();
+        String password = passwordField.getText();
+        String firstName = firstNameTextField.getText();
+        String lastName = lastNameTextField.getText();
+
+        try {
+            BankServiceUtil.getInstance().getUserService().register(login, password, firstName, lastName);
+        } catch (BankServiceException_Exception e) {
+            e.printStackTrace();
+        } catch (ValidationException_Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleCancelButton() {
+
+    }
+}
---
+++ src/main/java/pl/poznan/put/bsr/bank/front/views/RegisterDialogView.java
@@ -0,0 +1,34 @@
+package pl.poznan.put.bsr.bank.front.views;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+/**
+ * @author Kamil Walkowiak
+ */
+public class RegisterDialogView {
+    private Stage stage;
+
+    public RegisterDialogView() throws IOException {
+        stage = new Stage();
+        stage.initStyle(StageStyle.DECORATED);
+        stage.setResizable(false);
+        stage.initModality(Modality.NONE);
+        stage.setTitle("eBank - Register");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/RegisterDialog.fxml"));
+        Parent root = fxmlLoader.load();
+
+        stage.setScene(new Scene(root));
+    }
+
+    public void show() {
+        stage.show();
+    }
+}
