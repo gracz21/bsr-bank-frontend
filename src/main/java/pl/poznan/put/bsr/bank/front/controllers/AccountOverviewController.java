7f352b8e9f5d3b79c022e7f0dfaa362f9efc07e4
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-12-29 18:39:58 +0100

    Added working accounts table and add account function

--- src/main/java/pl/poznan/put/bsr/bank/front/controllers/AccountOverviewController.java
+++ src/main/java/pl/poznan/put/bsr/bank/front/controllers/AccountOverviewController.java
@@ -1,14 +1,71 @@
 package pl.poznan.put.bsr.bank.front.controllers;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextInputDialog;
 import pl.poznan.put.bsr.bank.front.utils.BankServiceUtil;
 import pl.poznan.put.bsr.bank.front.utils.InformationDialogsUtil;
 import pl.poznan.put.bsr.bank.services.AuthException_Exception;
+import pl.poznan.put.bsr.bank.services.BankAccount;
+import pl.poznan.put.bsr.bank.services.BankServiceException_Exception;
+import pl.poznan.put.bsr.bank.services.ValidationException_Exception;
+
+import java.util.List;
+import java.util.Optional;
+
 
 /**
  * @author Kamil Walkowiak
  */
 public class AccountOverviewController {
+    private ObservableList<BankAccount> bankAccounts;
+
+    @FXML
+    private TableView<BankAccount> accountsTableView;
+    @FXML
+    private TableColumn<BankAccount, String> nameTableColumn;
+    @FXML
+    private TableColumn<BankAccount, String> accountNoTableColumn;
+
+    public AccountOverviewController() {
+        List<BankAccount> response = null;
+        try {
+            response = BankServiceUtil.getInstance().getBankAccountService().getCurrentUserBankAccounts();
+        } catch (AuthException_Exception | BankServiceException_Exception e) {
+            InformationDialogsUtil.showExceptionDialog(e.getMessage());
+        }
+
+        bankAccounts = FXCollections.observableArrayList(response);
+    }
+
+    @FXML
+    private void initialize() {
+        nameTableColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName()));
+        accountNoTableColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getAccountNo()));
+
+        accountsTableView.setItems(bankAccounts);
+    }
+
+    @FXML
+    private void handleAddBankAccountButton() {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Bank account name");
+        dialog.setHeaderText("Input new bank account name");
+        dialog.setContentText("Name:");
+
+        Optional<String> result = dialog.showAndWait();
+        String name = result.get();
+        try {
+            bankAccounts.add(BankServiceUtil.getInstance().getBankAccountService().addBankAccount(name));
+        } catch (AuthException_Exception | BankServiceException_Exception | ValidationException_Exception e) {
+            InformationDialogsUtil.showExceptionDialog(e.getMessage());
+        }
+    }
+
     @FXML
     private void handleLogoutButton() {
         try {
