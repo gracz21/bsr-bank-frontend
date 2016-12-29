a73817252e6ebfce693ea418fbfe4b07e944c21e
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-12-29 21:09:05 +0100

    Added account history view

--- src/main/java/pl/poznan/put/bsr/bank/front/controllers/AccountOverviewController.java
+++ src/main/java/pl/poznan/put/bsr/bank/front/controllers/AccountOverviewController.java
@@ -1,5 +1,6 @@
 package pl.poznan.put.bsr.bank.front.controllers;
 
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -10,10 +11,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextInputDialog;
 import pl.poznan.put.bsr.bank.front.utils.BankServiceUtil;
 import pl.poznan.put.bsr.bank.front.utils.InformationDialogsUtil;
-import pl.poznan.put.bsr.bank.services.AuthException_Exception;
-import pl.poznan.put.bsr.bank.services.BankAccount;
-import pl.poznan.put.bsr.bank.services.BankServiceException_Exception;
-import pl.poznan.put.bsr.bank.services.ValidationException_Exception;
+import pl.poznan.put.bsr.bank.services.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -37,6 +35,20 @@ public class AccountOverviewController {
     private Label accountNoLabel;
     @FXML
     private Label balanceLabel;
+    @FXML
+    private TableView<BankOperation> historyTableView;
+    @FXML
+    private TableColumn<BankOperation, String> typeTableColumn;
+    @FXML
+    private TableColumn<BankOperation, String> titleTableColumn;
+    @FXML
+    private TableColumn<BankOperation, Double> amountTableColumn;
+    @FXML
+    private TableColumn<BankOperation, String> sourceTableColumn;
+    @FXML
+    private TableColumn<BankOperation, String> targetTableColumn;
+    @FXML
+    private TableColumn<BankOperation, Double> balanceAfterTableColumn;
 
     public AccountOverviewController() {
         List<BankAccount> response = null;
@@ -53,12 +65,8 @@ public class AccountOverviewController {
     private void initialize() {
         resetAccountDetails();
 
-        nameTableColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName()));
-        accountNoTableColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getAccountNo()));
-
-        accountsTableView.setItems(bankAccounts);
-        accountsTableView.getSelectionModel().selectedItemProperty().addListener(
-                (observable, oldValue, newValue) -> showAccountDetails(newValue));
+        initializeAccountTableView();
+        initializeHistoryTableView();
     }
 
     @FXML
@@ -87,15 +95,70 @@ public class AccountOverviewController {
         }
     }
 
+    private void initializeAccountTableView() {
+        nameTableColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName()));
+        accountNoTableColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getAccountNo()));
+
+        accountsTableView.setItems(bankAccounts);
+        accountsTableView.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> showAccountDetails(newValue));
+    }
+
+    private void initializeHistoryTableView() {
+        typeTableColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getClass().getSimpleName()));
+        titleTableColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTitle()));
+        amountTableColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getAmount()));
+        sourceTableColumn.setCellValueFactory(cellData -> new SimpleStringProperty(prepareSource(cellData.getValue())));
+        targetTableColumn.setCellValueFactory(cellData -> new SimpleStringProperty(prepareTarget(cellData.getValue())));
+        balanceAfterTableColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getBalanceAfter()));
+    }
+
+    private String prepareSource(BankOperation bankOperation) {
+        String source = null;
+        switch(bankOperation.getClass().getSimpleName()) {
+            case "Payment":
+                source = "N/A";
+                break;
+            case "Withdrawal":
+                source = bankOperation.getReceiverAccount();
+                break;
+            case "Transfer":
+                source = ((Transfer)bankOperation).getSenderAccount();
+                break;
+        }
+
+        return source;
+    }
+
+    private String prepareTarget(BankOperation bankOperation) {
+        String target = null;
+        switch(bankOperation.getClass().getSimpleName()) {
+            case "Payment":
+                target = bankOperation.getReceiverAccount();
+                break;
+            case "Withdrawal":
+                target = "N/A";
+                break;
+            case "Transfer":
+                target = bankOperation.getReceiverAccount();
+                break;
+        }
+
+        return target;
+    }
+
     private void resetAccountDetails() {
         nameLabel.setText(null);
         accountNoLabel.setText(null);
         balanceLabel.setText(null);
+        historyTableView.setItems(null);
     }
 
     private void showAccountDetails(BankAccount bankAccount) {
         nameLabel.setText(bankAccount.getName());
         accountNoLabel.setText(bankAccount.getAccountNo());
         balanceLabel.setText(bankAccount.getBalance() + " zł");
+        historyTableView.setItems(FXCollections.observableArrayList(
+                bankAccount.getHistory().getBankOperationOrPaymentOrTransfer()));
     }
 }
